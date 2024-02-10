83bd990f192f2ecd498d2ebe9e346fd5eba062ea
Federico467 <107440274+Federico467@users.noreply.github.com>
2024-02-10 16:55:13 +0100

    Delete build directory

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/lorenzo/FoR-project/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/lorenzo/FoR-project/devel;/home/lorenzo/prova/devel;/home/lorenzo/ros_ws/install;/opt/ros/noetic'.split(';') if '/home/lorenzo/FoR-project/devel;/home/lorenzo/prova/devel;/home/lorenzo/ros_ws/install;/opt/ros/noetic' != '' else []
--- build/motion/catkin_generated/motion-msg-extras.cmake.develspace.in
+++
@@ -1,2 +0,0 @@
-set(motion_MESSAGE_FILES "/home/lorenzo/FoR-project/src/motion/msg/pos.msg")
-set(motion_SERVICE_FILES "")
